a8d8cb4ab1680a17e9be7e7b9e360606c17f858b
Maarten Van Steenkiste <maarten.vansteenkiste@vlaanderen.be>
2026-02-26 14:03:10 +0100

    Update Next.js configuration for GitHub Pages deployment and add workflow for automated deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mint-and-elli-website',
 };
 
 export default nextConfig;
